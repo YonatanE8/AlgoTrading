52415b56ca7738d11b7deafdc252f4d34a88f99d
Yonatan <yonatan@sibylla.io>
2020-12-09 23:49:15 +0200

    Debugged the ARIMA prediction in the forecaster

--- scripts/plot_forecasts.py
+++ scripts/plot_forecasts.py
@@ -7,33 +7,19 @@ from src.visualizations.plot_assets import plot_forecasts
 import os
 
 # Define data parameters
-# symbols_list = ('MSFT', 'DIS', 'JPM', 'C', 'DAL')
-symbol = 'JPM'
+symbols_list = ('MSFT', 'DIS', 'JPM', 'C', 'DAL')
 start_date = "2015-12-03"
 end_date = "2020-12-03"
 quote_channels = ('Close',)
 adjust_prices = True
 cache_path = os.path.join(PROJECT_ROOT, 'data')
 
-# Get data
-quotes, macros = get_asset_data(symbol=symbol, start_date=start_date,
-                                end_date=end_date,
-                                quote_channels=quote_channels,
-                                adjust_prices=adjust_prices,
-                                cache_path=cache_path)
-assets_data = quotes[quote_channels[0]]
-period_length = 66
-n_periods = len(assets_data) // period_length
-periods = [assets_data[i * period_length:((i + 1) * period_length)]
-           for i in range(n_periods)]
-dates = quotes['Dates']
-
 # Define Smoother
 # smoother = Smoother(method='avg', length=15)
 # smoother = Smoother(method='exp', alpha=0.6, optimize=False)
 # smoother = Smoother(method='exp', optimize=True)
 # smoother = Smoother(method='holt_winter', trend=None)
-smoother = Smoother(method='polyfit', poly_degree=7)
+smoother = Smoother(method='polyfit', poly_degree=4)
 
 # Define Forecaster
 # method = 'arima'
@@ -52,8 +38,36 @@ alpha = 0.4
 save_plot_path = None
 save_report_path = None
 
-# Plot forecast
-plot_forecasts(periods=periods, smoother=smoother, forecaster=forecaster,
-               asset_symbol=symbol, dates=dates, asset_meta_data=macros,
-               save_plot_path=save_plot_path, save_report_path=save_report_path,
-               linewidth=linewidth, markersize=markersize, alpha=alpha)
+if __name__ == '__main__':
+    for symbol in symbols_list:
+        # Get data
+        quotes, macros = get_asset_data(symbol=symbol, start_date=start_date,
+                                        end_date=end_date,
+                                        quote_channels=quote_channels,
+                                        adjust_prices=adjust_prices,
+                                        cache_path=cache_path)
+        assets_data = quotes[quote_channels[0]]
+        period_length = 66
+        n_periods = len(assets_data) // period_length
+        periods = [assets_data[i * period_length:((i + 1) * period_length)]
+                   for i in range(n_periods)]
+        dates = quotes['Dates']
+
+        if save_plot_path is not None:
+            save_plot_path_symbol = os.path.join(save_plot_path, (symbol + '.png'))
+
+        else:
+            save_plot_path_symbol = None
+
+        if save_report_path is not None:
+            save_report_path_symbol = os.path.join(save_report_path, (symbol + '.png'))
+
+        else:
+            save_report_path_symbol = None
+
+        # Plot forecast
+        plot_forecasts(periods=periods, smoother=smoother, forecaster=forecaster,
+                       asset_symbol=symbol, dates=dates, asset_meta_data=macros,
+                       save_plot_path=save_plot_path_symbol,
+                       save_report_path=save_report_path_symbol,
+                       linewidth=linewidth, markersize=markersize, alpha=alpha)

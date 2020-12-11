105d4eb65fe36bfa579b74f365a72a5075333f04
Yonatan <yonatan@sibylla.io>
2020-12-11 19:06:13 +0200

    Continued the implementation of the Analyzer class, currently still needs to complete the _analyze_periodicty method

--- src/analysis/analyzing.py
+++ src/analysis/analyzing.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from scipy.signal import welch
 from src.io.data_queries import get_asset_data, get_multiple_assets
 
 import numpy as np
@@ -12,6 +13,7 @@ class Analyzer(ABC):
     def __init__(self, symbols_list: tuple, start_date: str, end_date: str = None,
                  quote_channel: str = 'Close', adjust_prices: bool = True,
                  risk_free_asset_symbol: str = '^IRX', bins: int = 10,
+                 spectral_energy_threshold: float = 0.2,
                  cache_path: str = None):
         """
         Constructor method for the Analyzer class
@@ -31,11 +33,14 @@ class Analyzer(ABC):
         asset (for example for Sharpe-Ratio computation). Defaults to '^IRX',
         which refers to the yield of US treasury bonds for 13 weeks.
         :param bins: (int) Number of bins to use in the histogram analysis computation.
+        :param spectral_energy_threshold: (float) threshold between 0 and 1 to use
+        when performing the periodicty analysis. Defaults to 0.2.
         :param cache_path: (str) Path to the directory in which to cache / look for
         cached data, if None does not use caching. Default is None.
         """
 
         # Setup
+        self.n_assets = len(symbols_list)
         self.symbols_list = symbols_list
         self.start_date = start_date
         self.end_date = end_date
@@ -43,6 +48,7 @@ class Analyzer(ABC):
         self.adjust_prices = adjust_prices
         self.risk_free_asset_symbol = risk_free_asset_symbol
         self.bins = bins
+        self.spectral_energy_threshold = spectral_energy_threshold
 
         # Query assets
         quotes = get_multiple_assets(symbols_list=symbols_list, start_date=start_date,
@@ -60,7 +66,10 @@ class Analyzer(ABC):
                                          adjust_prices=adjust_prices,
                                          cache_path=cache_path)
         self.risk_free_asset = risk_free_asset[quote_channel]
-        self.risk_free_returns = self._compute_returns(self.risk_free_asset)
+
+        # Risk-free asset is already reported as returns, remove the first entry so that
+        # the risk-free returns will align with the assets' returns
+        self.risk_free_returns = self.risk_free_asset[1:]
 
     def _compute_returns(self, quotes: np.ndarray) -> np.ndarray:
         """
@@ -96,6 +105,18 @@ class Analyzer(ABC):
 
         return sr
 
+    @property
+    def sr(self) -> np.ndarray:
+        """
+        Class property, the Sharpe-Ratio of the analyzed assets
+
+        :return: (np.ndarray) An array with shape (T - 2, # assets) containing the
+        Sharpe-Ratio of the analyzed assets. Where T is the temporal length of the
+        requested assets.
+        """
+
+        return self._analyze_sr()
+
     def _returns_histogram(self) -> (np.ndarray, np.ndarray):
         """
         A utility method for computing the histogram of returns.
@@ -109,8 +130,66 @@ class Analyzer(ABC):
 
         return values, counts
 
-    def _analyze_periodicty(self):
+    @property
+    def mean_annual_return(self) -> float:
+        """
+        Class property, denoting the mean annual return of each asset, where the
+        mean annual return is computed as the mean return over the entire
+        requested period, multiplied by 255 (trading days per year on avg.)
+
+        :return: (float) The mean annual return
+        """
+
+        # Get mean returns
+        mean_returns = np.mean(self.returns, axis=0)
+
+        # Annualize
+        mean_annual_returns = 255 * mean_returns
+
+        return mean_annual_returns
+
+    def _analyze_periodicty(self, quotes: np.ndarray) -> np.ndarray:
+        """
+        Performs a spectrum estimation for each asset using the Welch power-spectrum
+        density estimator. Then detect every spectral component which relative energy is
+        higher then the energy threshold component in the constructor.
+        We then compose a new signal consistent of:
+            S = sigma_{i = 1}^{n} E[i] * sin(2 * pi * Frequency[i])
+        where E[i] is the energy of the spectral component i, and Frequency[i] is
+        the frequency of the spectral component i.
+        Finally, the periodic signal is taken to be:
+            Y = S + Avg(x)
+        Where Avg(x) refers to taking the running mean over the raw signal.
+
+        :param quotes: (np.ndarray) The assets to analyze
+
+        :return: (np.ndarray) The final periodic signal
+        """
+
+        frequencies, power_spectrum = welch(quotes, fs=1.0, window='hann',
+                                            return_onesided=True, scaling='density',
+                                            axis=0)
+        total_energy = np.sum(power_spectrum, axis=0)
+        normalized_power_spectrum = power_spectrum / np.expand_dims(total_energy, 0)
+
+        spectral_components = [
+            np.where(normalized_power_spectrum[:, i] >
+                     self.spectral_energy_threshold)[0]
+            for i in range(self.n_assets)
+        ]
+
+        spectral_components_freqs = [
+            frequencies[component]
+            for component in spectral_components if len(component)
+        ]
+
+        spectral_components_energies = [
+            normalized_power_spectrum[component, i]
+            for i, component in enumerate(spectral_components) if len(component)
+        ]
+
         pass
+
 
 
 
